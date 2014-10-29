1b58360c50a21f94bc468f01bb9f06229b8da33e
Daniel Johnson <danieljohnson2@verizon.net>
2014-10-29 16:31:14 -0400

    The mod will now give 16 snowballs when players join or respawn, in the rightmost hotbar spot.

--- SnowballMadness/src/snowballmadness/SnowballMadness.java
+++ SnowballMadness/src/snowballmadness/SnowballMadness.java
@@ -4,8 +4,14 @@
  */
 package snowballmadness;
 
+import java.util.*;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.*;
 import org.bukkit.event.entity.*;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -31,5 +37,34 @@ public class SnowballMadness extends JavaPlugin implements Listener {
     @EventHandler
     public void onProjectileHit(ProjectileHitEvent e) {
         SnowballLogic.onProjectileHit(e);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        bestowSnowball(e.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        bestowSnowball(e.getPlayer());
+    }
+
+    /**
+     * This method gives a player a snowball in a designated snowball slot,
+     * provided this slot is empty.
+     *
+     * @param player The player to be gifted with snow!
+     */
+    @SuppressWarnings("deprecation")
+    private void bestowSnowball(Player player) {
+        PlayerInventory inventory = player.getInventory();
+        final int slotNumber = 8;
+
+        ItemStack oldStack = inventory.getItem(slotNumber);
+
+        if (oldStack == null || oldStack.getType() == Material.SNOW_BALL) {
+            inventory.setItem(slotNumber, new ItemStack(Material.SNOW_BALL, 16));
+            player.updateInventory();
+        }
     }
 }
