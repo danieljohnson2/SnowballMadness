e5747b95467e4c4ef56f1c568ed172f067fbc616
Applejinx <jinx6568@sover.net>
2014-11-07 15:47:40 -0500

    Bouncy Snowball logic, partly implemented. It is giving exactly one bounce: intent is for it to either bounce until there's solid blocks over where it hits (a wall, in other words) or bounce the number of slimeballs you have in the modifier slot. If the latter it has to jump up to the nearest airspace whenever it hits something solid.

---
+++ SnowballMadness/src/snowballmadness/BouncySnowballLogic.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snowballmadness;
+
+import com.google.common.base.Preconditions;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.util.*;
+
+/**
+ * This snowball logic spawns
+ *
+ * @author christopherjohnson
+ */
+public class BouncySnowballLogic extends SnowballLogic {
+
+    private final InventorySlice inventory;
+
+    public BouncySnowballLogic(InventorySlice inventory) {
+        this.inventory = Preconditions.checkNotNull(inventory);
+
+    }
+
+    @Override
+    public void hit(Snowball snowball, SnowballInfo info) {
+        super.hit(snowball, info);
+
+        World world = snowball.getWorld();
+        LivingEntity shooter = snowball.getShooter();
+        Location source = snowball.getLocation().clone();
+        source.setY(source.getY() + 0.25);
+
+        Vector bounce = snowball.getVelocity().clone();
+        bounce.setY(-(bounce.getY()));
+        //we are not going to amplify the bounce because the initial velocity should
+        //be what's amplified. Thus we needn't amplify it again.
+
+        Snowball skipper = world.spawn(source, Snowball.class);
+        skipper.setShooter(shooter);
+        skipper.setVelocity(bounce);
+
+        performLaunch(inventory, skipper, info);
+        //this should produce one bounce without any trouble
+        //two possibilities for completing it.
+        //One, it bounces forever until it hits somewhere with a solid block over where it hit
+        //Two, it bounces the number of slimeballs you have in there, and then goes off.
+        //I'm not sure quite how to make it 'launch itself' rather than a payload snowball,
+        //but it's probably trivial.
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -> (x%d) %s",
+                super.toString());
+    }
+}
