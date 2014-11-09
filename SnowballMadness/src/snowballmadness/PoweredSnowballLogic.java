ed07744dc5501d6a3c68919449f644115796370f
Applejinx <jinx6568@sover.net>
2014-11-09 13:08:41 -0500

    Powering snowballs with glowstone makes them heavier. If you use double glowstone and TNT, the snowball is so heavy you have to throw it at maximum range and duck back to survive. Anything nearer and you're toast. Lesser amounts of glowstone cut down the range to a smaller extent, double glowstone is the one that's tough. It's not linear with sugar/cake, cake will still get you much better range.

--- SnowballMadness/src/snowballmadness/PoweredSnowballLogic.java
+++ SnowballMadness/src/snowballmadness/PoweredSnowballLogic.java
@@ -19,6 +19,17 @@ public class PoweredSnowballLogic extends ChainableSnowballLogic {
         this.factor = factor;
     }
 
+    /**
+     * This returns the factor applied to speed, sp the power slows the
+     * snowball.
+     *
+     * @return The factor to apply to the speed.
+     */
+    private double getSpeedFactor() {
+        return 1.0 - (
+                factor / 18.0);
+    }
+
     @Override
     public double damage(Snowball snowball, SnowballInfo info, Entity target, double proposedDamage) {
         double damage = super.damage(snowball, info, target, proposedDamage);
@@ -26,13 +37,22 @@ public class PoweredSnowballLogic extends ChainableSnowballLogic {
         if (factor > 1.0 && damage == 0.0) {
             damage = 1.0;
         }
-        
+
         return damage * factor;
     }
 
     @Override
+    public void launch(Snowball snowball, SnowballInfo info) {
+        Vector v = snowball.getVelocity().clone();
+        v.multiply(info.speed * getSpeedFactor());
+        snowball.setVelocity(v);
+
+        super.launch(snowball, info);
+    }
+
+    @Override
     protected SnowballInfo adjustInfo(Snowball snowball, SnowballInfo info) {
-        return info.powered(factor);
+        return info.powered(factor).speeded(getSpeedFactor());
     }
 
     @Override
