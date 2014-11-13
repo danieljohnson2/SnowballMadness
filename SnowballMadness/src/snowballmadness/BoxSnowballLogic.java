e20d9f3b708e7d301bbb3ab1b501caa91bbea888
Daniel Johnson <danieljohnson2@verizon.net>
2014-11-13 05:39:54 -0500

    I've done cleanup on the BoxSnowballLogic; it can now be made to fill the interior with something other than air, or not fill the interior at all, and with subclassing it can be made to replace everything, or things other than air and liquids.
    I've also changed it to for-loops for iteration, and introduced more correct clamping logic.

--- SnowballMadness/src/snowballmadness/BoxSnowballLogic.java
+++ SnowballMadness/src/snowballmadness/BoxSnowballLogic.java
@@ -11,103 +11,101 @@ import org.bukkit.entity.*;
 
 /**
  * This logic makes a box out of the material you give it, varying the
- * construction method by type. For starters, it just makes a box in open
- * air blocks, replacing all water or lava inside it with air.
- * 
+ * construction method by type. You can specify the material for the walls and
+ * the fill, but by default this class will only replace air and liquid blocks.
+ *
  * @author christopherjohnson
  */
 public class BoxSnowballLogic extends SnowballLogic {
 
-    private final Material toPlace;
+    private final Material wallMaterial;
+    private final Material fillMaterial;
 
-    public BoxSnowballLogic(Material toPlace) {
-        this.toPlace = Preconditions.checkNotNull(toPlace);
+    public BoxSnowballLogic(Material wallMaterial) {
+        this(wallMaterial, Material.AIR);
+    }
+
+    public BoxSnowballLogic(Material wallMaterial, Material fillMaterial) {
+        // we allow nulls here, with the meaning 'don't touch the blocks at
+        // all'.
+        this.wallMaterial = wallMaterial;
+        this.fillMaterial = fillMaterial;
     }
 
     @Override
     public void hit(Snowball snowball, SnowballInfo info) {
         super.hit(snowball, info);
 
-        Location loc = snowball.getLocation().clone();
-        int dimensions = 1;
-        dimensions = (int) (dimensions * info.power);
-        //we're gonna loop and make a solid box, where possible in vertical strips
-        //in case Minecraft sends stuff piecemeal: not to repeatedly update multiple
-        //chunks for each layer.
+        Location snowballLoc = snowball.getLocation();
+        World world = snowball.getWorld();
+        final int radius = (int) info.power;
+        final int diameter = (int) (info.power * 2);
 
-        int beginX = loc.getBlockX() - dimensions;
-        int beginY = loc.getBlockY() - dimensions;
-        int beginZ = loc.getBlockZ() - dimensions;
-        int endX = beginX + (dimensions * 2);
-        int endY = beginY + (dimensions * 2);
-        int endZ = beginZ + (dimensions * 2);
-        if (beginY < 1) {
-            beginY = 1;
-        }
-        if (endY > 256) {
-            endY = 256;
-        }
-        //Y can go out of bounds, clamp it to permissible heights
-        //these are common to all box models, we can have some special cases
+        // while in theory x anx z are unlimited, we want to keep y
+        // within the normal world.
 
-        //CASE, hollow box whose shell does not replace anything but air
-        //air inside displaces water and lava
-        int iterateX = beginX;
-        while (iterateX <= endX) {
-            loc.setX(iterateX);
-            int iterateZ = beginZ;
-            while (iterateZ <= endZ) {
-                loc.setZ(iterateZ);
-                int iterateY = beginY;
-                if (iterateX == beginX || iterateX == endX || iterateZ == beginZ || iterateZ == endZ) {
-                    //we are doing a wall, hence we fill in all Y
-                    while (iterateY <= endY) {
-                        loc.setY(iterateY);
-                        Block target = loc.getBlock();
-                        if ((target.getType() == Material.AIR)||(target.getType() == Material.WATER)||(target.getType() == Material.STATIONARY_WATER)||(target.getType() == Material.LAVA)||(target.getType() == Material.STATIONARY_LAVA)) {
-                            target.setType(toPlace);
+        final int beginX = snowballLoc.getBlockX() - radius;
+        final int beginY = Math.max(1, snowballLoc.getBlockY() - radius);
+        final int beginZ = snowballLoc.getBlockZ() - radius;
+        final int endX = beginX + diameter;
+        final int endY = Math.min(world.getMaxHeight(), beginY + diameter);
+        final int endZ = beginZ + diameter;
+
+        // no worries- all this executes before Minecraft can send anything
+        // back to the client, so we can set the blocks in any order. This one
+        // is convenient!
+
+        for (int x = beginX; x <= endX; ++x) {
+            for (int z = beginZ; z <= endZ; ++z) {
+                for (int y = beginY; y <= endY; ++y) {
+                    final boolean isWallBlock =
+                            x == beginX || x == endX
+                            || z == beginZ || z == endZ
+                            || y == beginY || y == endY;
+
+                    final Material replacement = isWallBlock ? wallMaterial : fillMaterial;
+
+                    if (replacement != null) {
+                        Block target = world.getBlockAt(x, y, z);
+                        if (canReplace(target)) {
+                            replaceBlock(target, replacement);
                         }
-                        iterateY = iterateY + 1;
                     }
-                } else {
-                    //we aren't doing a wall, hence we fill in top and bottom blocks only
-                    //however, boxes can make air inside water or lava
-                    loc.setY(iterateY);
-                    Block target = loc.getBlock();
-                    if ((target.getType() == Material.AIR)||(target.getType() == Material.WATER)||(target.getType() == Material.STATIONARY_WATER)||(target.getType() == Material.LAVA)||(target.getType() == Material.STATIONARY_LAVA)) {
-                        target.setType(toPlace);
-                    }
-                    iterateY = iterateY + 1;
-                    //bottom plane
-                    while (iterateY < endY) {
-                        loc.setY(iterateY);
-                        target = loc.getBlock();
-                        if ((target.getType() == Material.AIR)||(target.getType() == Material.WATER)||(target.getType() == Material.STATIONARY_WATER)||(target.getType() == Material.LAVA)||(target.getType() == Material.STATIONARY_LAVA)) {
-                            target.setType(Material.AIR);
-                        }
-                        iterateY = iterateY + 1;
-                    }
-                    //hollow space, replaces water and lava, exits when top plane is reached
-                    loc.setY(iterateY);
-                    target = loc.getBlock();
-                    if ((target.getType() == Material.AIR)||(target.getType() == Material.WATER)||(target.getType() == Material.STATIONARY_WATER)||(target.getType() == Material.LAVA)||(target.getType() == Material.STATIONARY_LAVA)) {
-                        target.setType(toPlace);
-                    }
-                    //top plane and done with this vertical slice
                 }
-                iterateZ = iterateZ + 1;
-                //done with a Z row
             }
-            iterateX = iterateX + 1;
-            //done with an X row
         }
-        //end of hollow box case
-        
+    }
+
+    /**
+     * This method is called on each block that is to be updated, and by default
+     * simply calls setType() on nit. You can override this to set metadata or
+     * make other changes.
+     *
+     * This method is not called if the material would be null because null was
+     * passed tot he constructor. It is also nto called if canReplace() returns
+     * false.
+     *
+     * @param target The block to update.
+     * @param material The new material it should have.
+     */
+    protected void replaceBlock(Block target, Material material) {
+        target.setType(material);
+    }
+
+    /**
+     * This method decides whether to replace a given block; by default it
+     * replaces air and liquid blocks only.
+     *
+     * @param target The proposed block to update.
+     * @return True to update the block; false to do nothing.
+     */
+    protected boolean canReplace(Block target) {
+        Material material = target.getType();
+
+        return target.getType() == Material.AIR
+                || material == Material.WATER
+                || material == Material.STATIONARY_WATER
+                || material == Material.LAVA
+                || material == Material.STATIONARY_LAVA;
     }
 }
-
-//public boolean isFluid(Material toTest){
-//this.Material = target.getType();
-//return ((target.getType() == Material.AIR)||(target.getType() == Material.WATER)||(target.getType() == Material.STATIONARY_WATER)||(target.getType() == Material.LAVA)||(target.getType() == Material.STATIONARY_LAVA))
-//}
-//nope, but you can see what I'm going for here
