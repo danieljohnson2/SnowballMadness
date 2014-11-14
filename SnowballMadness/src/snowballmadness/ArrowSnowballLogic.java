2d668695dbc23354d4e1593239fb6ac9733ab6d2
Applejinx <jinx6568@sover.net>
2014-11-14 12:55:04 -0500

    More work on Arrow snowball. Location offset tweaked so you can't run into them normally, and effects added. If you're spamming fire arrows, a fire effect triggers, making a colorful display and explaining why the arrows caught fire. If you're firing sniper arrows, a bow sound is triggered. It's overlaid on itself for very high powers to produce a gunshot like effect that's not available through just setting playback volume (might be worth trying this for insane explosion effects: superhigh powers are LOUD)

--- SnowballMadness/src/snowballmadness/ArrowSnowballLogic.java
+++ SnowballMadness/src/snowballmadness/ArrowSnowballLogic.java
@@ -21,7 +21,6 @@ public class ArrowSnowballLogic extends SnowballLogic {
     public void launch(Snowball snowball, SnowballInfo info) {
         super.launch(snowball, info);
         World world = snowball.getWorld();
-
         Location location = snowball.getLocation().clone();
         Vector velocity = snowball.getVelocity();
 
@@ -30,9 +29,33 @@ public class ArrowSnowballLogic extends SnowballLogic {
         // side. If that happens, the client sees the arrow drop to
         // the ground.
 
-        location.add(velocity.normalize());
+        location.add(velocity.normalize().multiply(1.5));
+        //adjusting shot position so you can't run into it while firing
 
         float speed = (float) (info.speed);
+
+        if (speed > 1.7) {
+            snowball.getWorld().playEffect(location, Effect.BOW_FIRE, null, 128);
+            if (speed > 3.2) {
+                snowball.getWorld().playEffect(location, Effect.BOW_FIRE, null, 128);
+                if (speed > 4.7) {
+                    snowball.getWorld().playEffect(location, Effect.BOW_FIRE, null, 128);
+                    if (speed > 8.9) {
+                        snowball.getWorld().playEffect(location, Effect.BOW_FIRE, null, 128);
+                    }
+                }
+            }
+            //bow or better. Sound effect.
+            //by doing it this way, we are stacking bow shots on top of each other.
+            //that overrides the limitations on how loud it can be in Minecraft, producing
+            //a gunshot-like effect.
+        }
+
+        if (info.power > 1) {
+            snowball.getWorld().playEffect(location, Effect.MOBSPAWNER_FLAMES, null, 128);
+            //any power that causes arrows to be on fire
+        }
+        //special effects give feedback when the arrow is extreme: fast, or highpowered
 
         if (info.power > 1.0) {
             for (int i = 0; i < arrowCount; ++i) {
@@ -48,15 +71,14 @@ public class ArrowSnowballLogic extends SnowballLogic {
         snowball.remove();
     }
 }
-    
- /*   @Override
-        public void onProjectileHit(ProjectileHitEvent event) {
-            Entity entity = event.getEntity();
-            if (entity.getType() == EntityType.ARROW) {
-                entity.remove();
-            }
-        }  //this is outside what I understand, but was somebody's code for
-         // removing arrows stuck in the ground. I'd like our arrows to not
-         // leave tile entities stuck to blocks. More anti-lag cleanup.
-}
-} */
+/*   @Override
+ public void onProjectileHit(ProjectileHitEvent event) {
+ Entity entity = event.getEntity();
+ if (entity.getType() == EntityType.ARROW) {
+ entity.remove();
+ }
+ }  //this is outside what I understand, but was somebody's code for
+ // removing arrows stuck in the ground. I'd like our arrows to not
+ // leave tile entities stuck to blocks. More anti-lag cleanup.
+ }
+ } */
