34488db417e509b17369aaf811506735320be7a8
Daniel Johnson <danieljohnson2@verizon.net>
2014-11-14 05:04:50 -0500

    Bugfix: arrows snowballs fire from slightly further away, so they won't get caught on the shooter's hit-box.

--- SnowballMadness/src/snowballmadness/ArrowSnowballLogic.java
+++ SnowballMadness/src/snowballmadness/ArrowSnowballLogic.java
@@ -22,18 +22,27 @@ public class ArrowSnowballLogic extends SnowballLogic {
         super.launch(snowball, info);
         World world = snowball.getWorld();
 
+        Location location = snowball.getLocation().clone();
         Vector velocity = snowball.getVelocity();
+
+        // little tweak- move the arrow a little futher out so it
+        // does not interact with the shooter's hitbox on the client
+        // side. If that happens, the client sees the arrow drop to
+        // the ground.
+
+        location.add(velocity);
+
         float speed = (float) (info.speed);
 
         if (info.power > 1.0) {
             for (int i = 0; i < arrowCount; ++i) {
                 float spread = (float) ((arrowCount * 8) / info.power);
-                Arrow arrow = world.spawnArrow(snowball.getLocation(), velocity, speed, spread);
+                Arrow arrow = world.spawnArrow(location, velocity, speed, spread);
                 arrow.setShooter(snowball.getShooter());
                 arrow.setFireTicks((int) (info.power * 50));
             }
         } else {
-            Arrow arrow = world.spawnArrow(snowball.getLocation(), velocity, speed, 0);
+            Arrow arrow = world.spawnArrow(location, velocity, speed, 0);
             arrow.setShooter(snowball.getShooter());
         }
         snowball.remove();
