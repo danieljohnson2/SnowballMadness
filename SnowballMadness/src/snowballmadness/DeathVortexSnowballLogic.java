7dc9f7e30741515e8083ea81d3145ef18de9b904
Applejinx <jinx6568@sover.net>
2014-12-02 10:27:37 -0500

    For death vortex ball, the shooter's included in the 'snowball' category, almost totally immune to the magnetic effect. Therefore, it's not so necessary to seal yourself up to use it: it's a super-weapon after all!

--- SnowballMadness/src/snowballmadness/DeathVortexSnowballLogic.java
+++ SnowballMadness/src/snowballmadness/DeathVortexSnowballLogic.java
@@ -38,12 +38,12 @@ public class DeathVortexSnowballLogic extends SnowballLogic {
         }
 
         for (Entity victim : world.getEntities()) {
-            if (!(victim instanceof Snowball)) {
+            if ((victim instanceof Snowball) || (victim == snowball.getShooter())) {
+                accelerate(victim, target, (0.005 * info.power));
+                //victim is another snowball or the shooter, we make them slightly interactive
+            } else {
                 accelerate(victim, target, info.power);
                 accelerate(victim, previousTarget, info.power);
-            } else {
-                accelerate(victim, target, (0.005 * info.power));
-                //victim is another snowball, we make them slightly interactive
             }
         }
         previousTarget = target;
